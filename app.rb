4590e880e6160218e82a77105ea839bff927fb88
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-07-19 00:57:51 +0000

    multiple params

--- app.rb
+++ app.rb
@@ -57,9 +57,9 @@ get("/dynamic/50/6") do
   erb(:flexible)
 end
 
-get("/dynamic/:number_of_dice/6") do
+get("/dynamic/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
-  
+  @sides = params.fetch("how_many_sides").to_i
   @rolls = []
 
   @num_dice.times do
