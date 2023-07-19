738c394e41a20ac8110afe4c35bfc360fa6e82b1
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-07-19 00:19:20 +0000

    implemented dynamic

--- app.rb
+++ app.rb
@@ -56,3 +56,15 @@ get("/dynamic/50/6") do
 
   erb(:flexible)
 end
+
+get("/dynamic/:number_of_dice/6") do
+  @rolls = []
+
+  50.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
